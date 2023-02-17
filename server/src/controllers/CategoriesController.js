45bf00d70c12503740401ac302eac8291156ff93
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-17 17:37:07 -0300

    Functions for delete, list all and list only one categorie

--- server/src/controllers/CategoriesController.js
+++ server/src/controllers/CategoriesController.js
@@ -29,6 +29,76 @@ class CategoriesController {
 
     return response.status(204).json();
   }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const category = await knex("categories").where({ id }).first();
+
+    if (!category) {
+      throw new AppError("Categoria não encontrada.");
+    }
+
+    const subcategory = await knex("categories").where({ parentId: id }).first();
+
+    if (subcategory) {
+      throw new AppError("Categoria possui subcategorias.");
+    }
+
+    const articles = await knex("articles").where({ categoryId: id }).first();
+
+    if (articles) {
+      throw new AppError("Categoria possui artigos.");
+    }
+
+    await knex("categories").where({ id }).delete();
+
+    return response.json();
+  }
+
+  async index(request, response) {
+    function withPath(categories) {
+      function getParent(categories, parentId) {
+        const parent = categories.filter(parent => parent.id === parentId);
+        return parent.length ? parent[0] : null;
+      }
+
+      const categoriesWithPath = categories.map(category => {
+        let path = category.name;
+        let parent = getParent(categories, category.parentId);
+
+        while (parent) {
+          path = `${parent.name} >  ${path}`;
+          parent = getParent(categories, parent.parentId)
+        }
+        return { ...category, path }
+      })
+
+      categoriesWithPath.sort((category1, category2) => {
+        if (category1.path < category2.path) return -1;
+        if (category1.path > category2.path) return 1;
+        return 0;
+      });
+
+      return categoriesWithPath;
+    }
+
+    const categories = await knex("categories");
+
+    return response.json(withPath(categories));
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await knex("categories").where({ id }).first();
+
+    if (!category) {
+      throw new AppError("Categoria não encontrada.");
+    }
+
+    return response.json(category);
+  }
 }
 
 module.exports = CategoriesController;
--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -5,7 +5,10 @@ const CategoriesController = require("../controllers/CategoriesController");
 const categoryRoutes = Router();
 const categoriesController = new CategoriesController();
 
+categoryRoutes.get("/", categoriesController.index); 
+categoryRoutes.get("/:id", categoriesController.show); 
 categoryRoutes.post("/", categoriesController.create);
 categoryRoutes.put("/:id", categoriesController.update);
+categoryRoutes.delete("/:id", categoriesController.delete);
 
 module.exports = categoryRoutes;
