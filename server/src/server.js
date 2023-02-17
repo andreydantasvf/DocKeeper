98fe9677bd474edf2b7f11c6f75f6d119bcd095b
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-17 12:56:47 -0300

    Add dynamics messages of errors

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+const AppError = require("./utils/AppError");
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -8,6 +11,24 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(routes);
+
+// standardizing the errors
+app.use((error, request, response, next) => {
+  // comparing the error with AppError to statusCode 400
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.log(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+})
 
 const PORT = 3333;
 
