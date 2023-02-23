3ea010082bf205c4b3473bca74d83f251192eb1a
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-23 17:11:44 -0300

    Create route for get token auth

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const userRoutes = require("./user.routes");
 const categoryRoutes = require("./category.routes");
 const articleRoutes = require("./articles.routes");
+const sessionRoutes = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/categories", categoryRoutes);
 routes.use("/articles", articleRoutes);
+routes.use("/sessions", sessionRoutes);
 
 module.exports = routes;
