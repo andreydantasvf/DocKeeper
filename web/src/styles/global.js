7eb1d421690b99a36bd144b9fc008ef57c812f4a
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-27 18:04:10 -0300

    Create base react app

---
+++ web/src/styles/global.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 16.5%;
+  }
+
+  body {
+    background-color: #efefef;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body,
+  input,
+  button,
+  textarea {
+    font-size: 1.6rem;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #FFF;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+  
+  button:hover, a:hover {
+    filter: brightness(.9);
+  }
+`;
