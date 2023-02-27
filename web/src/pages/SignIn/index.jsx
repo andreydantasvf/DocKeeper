325b4b40f29cde3b698efaf433531b1a97f26a4a
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-27 20:05:33 -0300

    Create SignIn page

--- web/src/pages/SignIn/index.jsx
+++ web/src/pages/SignIn/index.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import { Container, Form } from "./styles";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { useAuth } from "../../hooks/auth";
+
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  async function handleSignIn(event) {
+    event.preventDefault();
+    signIn({ email, password });
+  }
+
   return (
-    <h1>SignIn</h1>
+    <Container>
+      <h1>DocKeeper</h1>
+
+      <Form>
+        <h2>Faça login</h2>
+        <Input
+          type="text"
+          placeholder="Exemplo: teste@gmail.com"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Exemplo: teste123"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button
+          title="Entrar"
+          loading={false}
+          type="submit"
+          onClick={handleSignIn}
+        />
+      </Form>
+    </Container>
   )
 }
