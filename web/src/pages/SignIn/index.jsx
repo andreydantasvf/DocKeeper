c4b9e3658abbeec8f9fcb8c1e709e30990ba7484
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-27 22:31:35 -0300

    Add link to /register

--- web/src/pages/SignIn/index.jsx
+++ web/src/pages/SignIn/index.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import { Container, Form } from "./styles";
 
 import { Input } from "../../components/Input";
@@ -9,12 +11,15 @@ import { useAuth } from "../../hooks/auth";
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const { signIn } = useAuth();
 
   async function handleSignIn(event) {
+    setIsLoading(true);
     event.preventDefault();
-    signIn({ email, password });
+    await signIn({ email, password });
+    setIsLoading(false);
   }
 
   return (
@@ -25,20 +30,23 @@ export function SignIn() {
         <h2>Faça login</h2>
         <Input
           type="text"
-          placeholder="Exemplo: teste@gmail.com"
+          placeholder="E-mail"
           onChange={e => setEmail(e.target.value)}
         />
         <Input
           type="password"
-          placeholder="Exemplo: teste123"
+          placeholder="Senha"
           onChange={e => setPassword(e.target.value)}
         />
         <Button
           title="Entrar"
-          loading={false}
+          loading={isLoading}
           type="submit"
           onClick={handleSignIn}
         />
+        <Link to="/register">
+          Não possui uma conta? Crie aqui!
+        </Link>
       </Form>
     </Container>
   )
