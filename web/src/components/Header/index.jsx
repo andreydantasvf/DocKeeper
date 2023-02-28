7302c8f9b288dfdd5c5005e07700f01bcdbb2061
Andrey Dantas <andreydantasvf@gmail.com>
2023-02-28 19:18:02 -0300

    Create UserMenu and logOut

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -1,25 +1,48 @@
+import {  useNavigate } from "react-router-dom";
 import { AiOutlineDown, AiOutlineRight } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../../redux/menuSlice";
 
-import { Container } from "./styles";
+import { useAuth } from "../../hooks/auth";
+
+import { Container, UserMenu, DropDown } from "./styles";
 
 export function Header({ hideToggle = false }) {
+  const { user, signOut } = useAuth();
   const dispatch = useDispatch();
   const { isMenuVisible } = useSelector(state => state.menu)
+
+  const navigation = useNavigate();
 
   function handleToggleMenu() {
     dispatch(toggleMenu());
   }
 
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
+
   return (
     <Container>
       {!hideToggle &&
-        <div onClick={handleToggleMenu}>
+        <div className="toggle-menu" onClick={handleToggleMenu}>
           {isMenuVisible ? <AiOutlineDown /> : <AiOutlineRight />}
         </div>
       }
+
       <h1>DocKeeper</h1>
+
+      <UserMenu>
+        <span>{user.name}</span>
+        <FaUserCircle />
+
+        <DropDown>
+          <a href="#">Configurações</a>
+          <a onClick={handleSignOut}>Sair</a>
+        </DropDown>
+      </UserMenu>
     </Container>
   )
 }
--- web/src/components/Header/styles.js
+++ web/src/components/Header/styles.js
@@ -12,10 +12,8 @@ export const Container = styled.header`
   justify-content: center;
 
   > div {
-    width: 6rem;
     height: 100%;
     color: #FFF;
-    font-size: 2rem;
 
     display: flex;
     align-items: center;
@@ -28,11 +26,73 @@ export const Container = styled.header`
     }
   }
 
+  > .toggle-menu {
+    width: 6rem;
+
+    font-size: 2rem;
+  }
+
   > h1 {
     font-size: 3rem;
     color: #FFF;
     font-weight: 100;
     flex-grow: 1;
     text-align: center;
+  }
+`;
+
+export const UserMenu = styled.div`
+  padding: 2rem;
+  gap: 1rem;
+  font-size: 2rem;
+
+  position: relative;
+
+  > span {
+    font-size: 1.4rem;
+    display: none;
+
+    @media(min-width: 768px) {
+      display: inline;
+    }
+  }
+
+  &:hover{
+    > div {
+      display: flex;
+    }
+  }
+`;
+
+export const DropDown = styled.div`
+  width: 17rem;
+  padding: 1rem;
+
+  position: absolute;
+  top: 100%;
+  right: 0;
+  z-index: 1;
+
+  display: none;  
+  flex-direction: column;
+  align-items: center;
+
+  background-color: #F9F9F9;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+
+  > a {
+    width: 100%;
+    font-size: 1.6rem;
+    text-align: center;
+    padding: 1rem;
+    word-break: break-all;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+  }
+
+  @media(min-width: 768px) {
+    width: 100%;
   }
 `;
