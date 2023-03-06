7689c02e66f4c86ecee7e2de6cb876fafaaeec70
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-06 11:33:58 -0300

    Initializing isMenuVisible = true

--- web/src/redux/menuSlice.js
+++ web/src/redux/menuSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const slice = createSlice({
   name: 'menu',
   initialState: {
-    isMenuVisible: true
+    isMenuVisible: false
   },
   reducers: {
     toggleMenu(state) {
