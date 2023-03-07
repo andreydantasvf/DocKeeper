89a68fc7b03204ea3035f0552b369a1cf4d968c6
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-07 15:30:33 -0300

    Adding Tree Menu with categories

--- web/src/components/Menu/index.jsx
+++ web/src/components/Menu/index.jsx
@@ -1,11 +1,34 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TreeMenu from 'react-simple-tree-menu'
+import { handleCategory, handleName } from "../../redux/categorySlice";
+
+import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
 export function Menu() {
+  const [data, setData] = useState(null);
   const { isMenuVisible } = useSelector(state => state.menu);
+  const dispatch = useDispatch();
+
+  function handleCategoryMenu(id, name) {
+    dispatch(handleCategory(id));
+    dispatch(handleName(name));
+  }
+
+  useEffect(() => {
+    async function fetchTree() {
+      const response = await api.get("/categories/tree");
+      setData(response.data)
+    }
+
+    fetchTree();
+  }, []);
 
   return (
-    isMenuVisible && <Container />
+    isMenuVisible && <Container>
+      <TreeMenu data={data} onClickItem={({ id, name }) => handleCategoryMenu(id, name)} />
+    </Container>
   )
 }
--- web/src/components/Menu/styles.js
+++ web/src/components/Menu/styles.js
@@ -6,6 +6,39 @@ export const Container = styled.aside`
   background: linear-gradient(to right, #232526, #414345);
 
   display: flex;
+  align-items: center;
   flex-direction: column;
   flex-wrap: wrap;
+
+  > div {
+    margin-top: 2rem;
+
+    input {
+      background: none;
+      border: none;
+      padding: 2rem;
+      border-bottom: 1px solid #FFF;
+      color: #FFF;
+    }
+
+    ul {
+      margin-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      li {
+        display: flex;
+        gap: 1rem;
+        padding: 1rem 2rem;
+        color: #FFF;
+
+        cursor: pointer;
+
+        &:hover {
+          background-color: #414345;
+        }
+      }
+    }
+  }
 `;
