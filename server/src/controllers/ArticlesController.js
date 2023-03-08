6d37422a15dc5f90ce863c603f5a454565ff6233
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-08 14:25:33 -0300

    Remove routes index and update in ArticlesController

--- server/src/controllers/ArticlesController.js
+++ server/src/controllers/ArticlesController.js
@@ -17,21 +17,6 @@ class ArticlesController {
     return response.status(201).json();
   }
 
-  async update(request, response) {
-    const articleUpdate = { ...request.body };
-    const { id } = request.params;
-
-    const article = await knex("articles").where({ id }).first();
-
-    if (!article) {
-      throw new AppError("Artigo não encontrado.");
-    }
-
-    await knex("articles").where({ id }).update(articleUpdate);
-
-    return response.status(204).json();
-  }
-
   async delete(request, response) {
     const { id } = request.params;
 
@@ -44,21 +29,6 @@ class ArticlesController {
     await knex("articles").where({ id }).delete();
 
     return response.status(204).json();
-  }
-
-  async index(request, response) {
-    const limit = 10; // usage for pagination
-
-    const page = request.query.page || 1;
-
-    const result = await knex("articles").count('id').first();
-    const count = parseInt(result.count);
-
-    const articles = await knex("articles")
-      .select('id', 'name', 'description')
-      .limit(limit).offset(page * limit - limit);
-
-    return response.json({ data: articles, count, limit });
   }
 
   async show(request, response) {
@@ -76,7 +46,6 @@ class ArticlesController {
   }
 
   async showByCategory(request, response) {
-    const page = request.query.page || 1;
     const categoryId = request.params.id;
     const categories = await knex.raw(
       `
@@ -92,7 +61,6 @@ class ArticlesController {
 
     const articles = await knex({ a: "articles", u: "users" })
       .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
-      .limit(10).offset(page * 10 - 10)
       .whereRaw('?? = ??', ['u.id', 'a.userId'])
       .whereIn('categoryId', ids)
       .orderBy('a.id', 'desc');
--- server/src/routes/articles.routes.js
+++ server/src/routes/articles.routes.js
@@ -8,10 +8,9 @@ const articlesController = new ArticlesController();
 
 articleRoutes.use(ensureAuthenticated);
 
-articleRoutes.get("/", articlesController.index);
+articleRoutes.get("/category/:id", articlesController.showByCategory);
 articleRoutes.get("/:id", articlesController.show);
 articleRoutes.post("/", articlesController.create);
-articleRoutes.put("/:id", articlesController.update);
 articleRoutes.delete("/:id", articlesController.delete);
 
 module.exports = articleRoutes;
