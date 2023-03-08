605c2bc118a0972c148c0c3724b1775f124c8613
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-08 13:29:14 -0300

    Create component BackButon

---
+++ web/src/components/BackButton/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  > a {
+    width: fit-content;
+    border-radius: .8rem;
+    margin-top: 2rem;
+    color: #FFF;
+    background: #464242;
+    padding: 1.3rem 2rem;
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    gap: .8rem;
+  }
+  > svg {
+    font-size: 2.4rem;
+    color: #FFF;
+  }
+`;
